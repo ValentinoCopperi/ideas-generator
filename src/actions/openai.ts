469d4a88f330f83fb42f22647860a6215b97079b
copep <valencope100@gmail.com>
2025-01-09 07:53:13 -0300

    open ai model changed

--- src/actions/openai.ts
+++ src/actions/openai.ts
@@ -24,7 +24,7 @@ export const genereteIdeas = async (data: ResponseData): Promise<Ideas> => {
         const { type, difficulty, theme, additionalInfo } = data;
 
         const response = await openai.chat.completions.create({
-            model: "gpt-4",
+            model: "gpt-4o",
             messages: [
                 {
                     role: "system", content: systemMessage
