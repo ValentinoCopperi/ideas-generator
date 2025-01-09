b640f23787f4a6bcc78e017e6b4c61d48d8c4bab
copep <valencope100@gmail.com>
2025-01-09 07:26:40 -0300

    deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -49,7 +49,7 @@ const handler = NextAuth({
                 if (result?.errors === undefined) {
                     return true
                 } else {
-                    console.error("Errore registering user:", result?.message)
+                    console.error("Error registering user:", result?.message)
                     return false
                 }
             }
